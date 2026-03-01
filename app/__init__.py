462e8d461ad6f2c90d13b2fe7d824d5ccef0af66
ROSHAN-KHANDAGALE <roshan15khandagale@gmail.com>
2026-03-02 00:34:38 +0530

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from dotenv import load_dotenv
+from app.db import init_db
+from app.routes.webhook import webhook_bp
+from app.routes.ui import ui_bp
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+
+    # Initialize MongoDB
+    init_db()
+
+    # Import blueprints INSIDE function (important!)
+    app.register_blueprint(webhook_bp)
+    app.register_blueprint(ui_bp)
+    
+    @app.route("/")
+    def health():
+        return {"status": "OK", "message": "Flask app is running"}
+
+    return app
